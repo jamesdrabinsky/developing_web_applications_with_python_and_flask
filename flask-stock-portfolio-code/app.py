3c50745b3cf4432f1195deb7f79322850f6cccbe
James Drabinsky <james.drabinsky@gmail.com>
2022-07-25 20:22:02 -0700

    Completed 'Routing' chapter in part 1.

--- flask-stock-portfolio-code/app.py
+++ flask-stock-portfolio-code/app.py
@@ -1,8 +1,30 @@
-from flask import Flask
+from flask import Flask, escape, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return "Hello World."
+    return render_template('index.html')
+
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html', company_name='TestDriven.io')
+
+
+@app.route('/stocks/', methods=['GET'])
+def stocks():
+    return '<h2>Stock List...</h2>'
+
+
+@app.route('/hello/<message>', methods=['GET'])
+def hello_message(message):
+    return f'<h1>Welcome {escape(message)}!</h1>'
+
+
+@app.route('/blog_posts/<int:post_id>', methods=['GET'])
+def display_blog_posts(post_id):
+    return f'<h1>Blog Post #{post_id}...</h1>'
+
+
